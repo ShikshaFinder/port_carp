7c1e01d8cd5a6de582072b37f605b4003a54c856
ShikshaFinder <janiharsh794@gmail.com>
2024-10-14 19:47:33 +0530

    responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
-import {FileUpload} from "../components/ui/file-upload";
-import React from 'react'
-// import {FlipWords} from "../components/ui/flip-words";
-import {BackgroundLines} from "../components/ui/background-lines";
+import { FileUpload } from "../components/ui/file-upload";
+import React from 'react';
+import { BackgroundLines } from "../components/ui/background-lines";
 
 function index() {
-    const words = ["better", "cute", "beautiful", "modern"];
+  // const words = ["better", "cute", "beautiful", "modern"];
 
   return (
     <>
-      <div className="h-[40rem] flex justify-center items-center px-4">
+      <div className="h-[40rem] flex flex-col md:flex-row justify-center items-center px-4">
         <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
           <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
             Talk To Your Files <br /> Any Time , Any Where
@@ -18,7 +17,7 @@ function index() {
           </p>
         </BackgroundLines>
 
-        <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-900 rounded-lg">
+        <div className="w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-900 rounded-lg mt-4 md:mt-0">
           <FileUpload />
         </div>
       </div>
@@ -26,4 +25,4 @@ function index() {
   );
 }
 
-export default index
+export default index;
